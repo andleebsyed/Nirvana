8523fe9046ac657b1f24d66e9f6b46fad01814db
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-06 00:22:14 +0530

    update : used grid for main page layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import { Header } from './components/Header/Header'
 import { Sidebar } from './components/Sidebar/Sidebar'
+import { Main } from './components/Main/Main'
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Sidebar />
+    <div className="main-outer-div">
+      <div className="header"><Header /></div>
+      <div className="sidebar"><Sidebar /></div>
+      <div className="main"><Main /></div>
+
+      <div></div>
     </div>
   );
 }
