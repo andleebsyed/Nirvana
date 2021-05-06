f272d87e73354271109cd08110a64aa4c62dc3e7
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-06 20:15:30 +0530

    update : made like responsive to user actions

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import './Sidebar.css'
+import { Link } from 'react-router-dom'
 export function Sidebar() {
     return (
         <div className="sidebar">
@@ -10,6 +11,15 @@ export function Sidebar() {
                 <a className="adjustment"><ion-icon name="compass-outline"></ion-icon></a><span className="adjustment icon-text">Explore</span>
 
             </div>
+            <Link to='/liked'>
+                <div className="side-icon">
+                    <a className="adjustment"><ion-icon name="thumbs-up-outline"></ion-icon></a><span className="adjustment icon-text">Liked</span>
+
+                </div>
+            </Link>
+
         </div>
     )
 }
+
+
