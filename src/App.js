3153159a7871080f39676d40482dacf1fa07f1ad
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-06 17:13:03 +0530

    refactor : moved some components into single homepage component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { Header } from './components/Header/Header'
-import { Sidebar } from './components/Sidebar/Sidebar'
-import { Main } from './components/Main/Main'
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { VideoPlayer } from './components/VideoPlayer/VideoPlayer';
+import { Homepage } from './components/Homepage/Homepage';
 function App() {
   return (
-    <div className="main-outer-div">
+    <div className>
       <div className="header"><Header /></div>
-      <div className="sidebar"><Sidebar /></div>
-      <div className="main"><Main /></div>
-
-      <div></div>
+      <Routes>
+        <Route path='/' element={<div><Homepage /></div>} />
+        <Route path="watch/:id" element={<div className=""><VideoPlayer /></div>} />
+      </Routes>
     </div>
   );
 }
