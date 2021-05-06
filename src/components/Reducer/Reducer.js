4e25eb405e9195fba62490b5ca99b911f87b7c94
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-06 20:14:53 +0530

    update : Reducer component to handle various data

---
+++ src/components/Reducer/Reducer.js
@@ -0,0 +1,30 @@
+import { useReducer, useContext, createContext } from 'react'
+
+const VideoHandleContext = createContext();
+
+function videosHandler(state, { type, payload }) {
+    const { likedVideos } = state
+    switch (type) {
+        case 'ADD_TO_LIKED_VIDEOS':
+            return { ...state, likedVideos: [...likedVideos, payload] }
+        case 'REMOVE_FROM_LIKED_VIDEOS':
+            return { ...state, likedVideos: likedVideos.filter(video => video.id !== payload.id) }
+    }
+}
+
+export function DataProvider({ children }) {
+
+    const likedVideos = []
+    const watchLaterVideos = []
+    const [state, dispatch] = useReducer(videosHandler, { likedVideos, watchLaterVideos })
+    console.log("state is ", state)
+    return (
+        <VideoHandleContext.Provider value={{ state, dispatch }}>
+            {children}
+        </VideoHandleContext.Provider>
+    )
+}
+
+export function useVideoData() {
+    return useContext(VideoHandleContext);
+}
