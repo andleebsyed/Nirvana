a8955f821076d6a88bbd5d43ccd41d58ea744ed1
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-11 00:45:18 +0530

    refactor : refactored homepage to  fit in grid structure which is defined

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,20 +1,11 @@
 import './Homepage.css'
 import { Categories } from '../Categories/Categories'
-import { Sidebar } from '../Sidebar/Sidebar'
 import { Main } from '../Main/Main'
-import { Liked } from '../Liked/Liked'
-import { Route, Routes } from 'react-router-dom'
 export function Homepage() {
     return (
-        <div className="homepage-outer">
-            {/* <div className="main"><Main /></div> */}
-            <Routes>
-                <Route path='/' element={<div className="main"><Main /></div>} />
-                <Route path='/liked' element={<div className="main"><Liked /></div>} />
-            </Routes>
-
+        <div>
             <div className="categories"><Categories /></div>
-            <div className="sidebar"><Sidebar /></div>
+            <div className="main"><Main /></div>
         </div>
     )
 }
