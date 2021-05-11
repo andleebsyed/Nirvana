a04c076e1d3d19bd4de4035b9168c225f9a060ba
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-11 17:42:01 +0530

    made notes component working

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,4 +1,5 @@
 import './VideoPlayer.css'
+import { useState } from 'react'
 import ReactPlayer from 'react-player'
 import { useParams } from 'react-router-dom'
 import { useVideo } from '../Data/Data'
@@ -11,6 +12,13 @@ export function VideoPlayer() {
     const currentVideo = videos.filter(video => video.id === id)
     const video = currentVideo[0];
     const videoInLiked = likedVideos.filter(videoInIteration => videoInIteration.id === video.id)
+    const [notes, setNotes] = useState([])
+
+    function notesHandler(e) {
+        setNotes([...notes, e.target.value])
+        e.currentTarget.value = "";
+    }
+
     return (
         <div className="outer-main">
             <div className="player-card">
@@ -18,15 +26,54 @@ export function VideoPlayer() {
                     <ReactPlayer
                         url={video.url}
                         controls={true}
+                        width={'50vw'}
+                        height={'60vh'}
                     />
                 </div>
-                {videoInLiked.length === 0 ?
-                    <button onClick={() => dispatch({ type: 'ADD_TO_LIKED_VIDEOS', payload: video })}>Like</button>
+                <div className="video-label">
+                    <p className="video-intro">
+                        {video.videoName}
+                    </p>
 
-                    :
-                    <button onClick={() => dispatch({ type: 'REMOVE_FROM_LIKED_VIDEOS', payload: video })}>Dislike</button>
-                }
-                <button onClick={() => dispatch({ type: 'REMOVE_FROM_LIKED_VIDEOS', payload: video })}>Add to Watch Later</button>
+                    <div className="interactions">
+                        {videoInLiked.length === 0 ?
+                            <button title="Like" onClick={() => dispatch({ type: 'ADD_TO_LIKED_VIDEOS', payload: video })} className='not-liked buttons' ><ion-icon name="thumbs-up-outline"></ion-icon></button>
+
+                            :
+                            <button onClick={() => dispatch({ type: 'REMOVE_FROM_LIKED_VIDEOS', payload: video })} className='liked buttons'><ion-icon name="thumbs-up-sharp"></ion-icon></button>
+                        }
+
+                        <button title="Add to Library" className="buttons tooltip" ><ion-icon name="create-outline"></ion-icon></button>
+
+
+                    </div>
+
+                </div>
+                <div className="creator-details">
+                    <div className="thumbnail-and-name">
+                        < img className="thumbnail" src={video.creatorThumbnail} />
+                        <p className="name">{video.creatorName}</p>
+                    </div>
+                    <div className="video-details">
+                        <p className="video-description">{video.description}</p>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+            <div className="notes-outer">
+                <p className="notes-heading">Take Notes</p>
+                <div className="notes">
+
+                    {notes.map(note => <div className="individual-note">
+                        {note}
+                    </div>)}
+                </div>
+                <input className="notes-input-box" type="text" placeholder="Type here..." onKeyPress={(e) => e.key === 'Enter' ? notesHandler(e) : 'do nothing'} />
+
+
             </div>
 
 
