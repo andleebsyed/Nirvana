902569e2453c943a417f1695ca70fa0c8d3da646
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-12 15:21:25 +0530

    update : removed Liked Button from sidebar , will be accessible from Library now

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -3,20 +3,15 @@ import { Link } from 'react-router-dom'
 export function Sidebar() {
     return (
         <div className="sidebar">
-            <div className="side-icon">
-                <a className="adjustment"><ion-icon name="home-outline"></ion-icon></a><span className="adjustment icon-text">Home</span>
-
-            </div>
-            <Link to='/explore' className="sidebar-link">
+            <Link to='/' className="sidebar-link">
                 <div className="side-icon">
-                    <a className="adjustment"><ion-icon name="compass-outline"></ion-icon></a><span className="adjustment icon-text">Explore</span>
+                    <a className="adjustment"><ion-icon name="home-outline"></ion-icon></a><span className="adjustment icon-text">Home</span>
 
                 </div>
             </Link>
-
-            <Link to='/liked' className="sidebar-link">
+            <Link to='/explore' className="sidebar-link">
                 <div className="side-icon">
-                    <a className="adjustment"><ion-icon name="thumbs-up-outline"></ion-icon></a><span className="adjustment icon-text">Liked</span>
+                    <a className="adjustment"><ion-icon name="compass-outline"></ion-icon></a><span className="adjustment icon-text">Explore</span>
 
                 </div>
             </Link>
