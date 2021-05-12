acb9f8228dd7809bca33ee50041cabcfb40d2ef3
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-12 15:20:38 +0530

    update : added homepage component

--- src/App.js
+++ src/App.js
@@ -3,28 +3,23 @@ import { Header } from './components/Header/Header'
 import { Liked } from './components/Liked/Liked'
 import { Routes, Route } from 'react-router-dom'
 import { VideoPlayer } from './components/VideoPlayer/VideoPlayer';
-import { Homepage } from './components/Homepage/Homepage';
+import { Explore } from './components/Explore/Explore';
 import { Sidebar } from './components/Sidebar/Sidebar';
 import { Library } from './components/Library/Library';
+import { Homepage } from './components/Homepage/Homepage';
 function App() {
-  // let currentClass = 'main-outer-div'
-  // if(path!== '/'){
-  //   currentClass = " dynamic-class"
-  // }
-  // else{
-  //   currentClass = "main-outer-div"
-  // }
-
   return (
     <div className="main-outer-div">
       <div className="header"><Header /></div>
       <div className="sidebar"><Sidebar /></div>
 
       <Routes>
-        <Route path='/' element={<div className="main"><Homepage /></div>} />
+        <Route path="/" element={<div className="main"> <Homepage /></div>} />
+        <Route path='/explore' element={<div className="main"><Explore /></div>} />
         <Route path='/liked' element={<div className="main"><Liked /></div>} />
         <Route path="watch/:id" element={<div className="main"><VideoPlayer /></div>} />
         <Route path='library' element={<div className="main"><Library /></div>} />
+
       </Routes>
 
     </div>
