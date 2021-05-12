d15a8aa46df5ce0d14da885f5b1d34faaba6f273
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-12 15:01:54 +0530

    updat e: created component for Playlists

---
+++ src/components/Playlists/Playlists.js
@@ -0,0 +1,8 @@
+export function Playlists() {
+    return (
+        <div className="playlists">
+            <h1 style={{ fontWeight: 'lighter', textAlign: 'left' }} >User Playlists</h1>
+
+        </div>
+    )
+}
