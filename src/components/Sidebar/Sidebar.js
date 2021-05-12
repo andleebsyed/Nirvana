60f511a57bd98a69df47b86ef09c8a140fff6f20
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-12 15:08:23 +0530

    style : added some styling to sidebar icons

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -7,17 +7,20 @@ export function Sidebar() {
                 <a className="adjustment"><ion-icon name="home-outline"></ion-icon></a><span className="adjustment icon-text">Home</span>
 
             </div>
-            <div className="side-icon">
-                <a className="adjustment"><ion-icon name="compass-outline"></ion-icon></a><span className="adjustment icon-text">Explore</span>
+            <Link to='/explore' className="sidebar-link">
+                <div className="side-icon">
+                    <a className="adjustment"><ion-icon name="compass-outline"></ion-icon></a><span className="adjustment icon-text">Explore</span>
 
-            </div>
-            <Link to='/liked'>
+                </div>
+            </Link>
+
+            <Link to='/liked' className="sidebar-link">
                 <div className="side-icon">
                     <a className="adjustment"><ion-icon name="thumbs-up-outline"></ion-icon></a><span className="adjustment icon-text">Liked</span>
 
                 </div>
             </Link>
-            <Link to='/library'>
+            <Link to='/library' className="sidebar-link">
                 <div className="side-icon">
                     <a className="adjustment"><ion-icon name="library-outline"></ion-icon></a><span className="adjustment icon-text">Library</span>
 
