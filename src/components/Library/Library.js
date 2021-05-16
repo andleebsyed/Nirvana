b293382ad3ac70c614f7d6c1267e1dc211f2838d
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-16 23:41:16 +0530

    update : added some ui to the condition when there are no playlists present and user checks for playlists

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -1,16 +1,23 @@
 import './Library.css'
 import { Liked } from '../Liked/Liked'
 import { Playlists } from '../Playlists/Playlists'
+import { useVideo } from '../Reducer/Reducer'
 export function Library() {
+    const { state } = useVideo()
+    const { playlists } = state
+    console.log("playlists in state are ", playlists)
     return (
         <div>
             <div className="liked-videos library-section">
                 <h1 className="heading-intro">Liked Videos|</h1>
                 <Liked />
             </div>
-            <div className="playlists library-section">
+            <div className="library-section">
                 <h1 className="heading-intro">Playlists|</h1>
-                <Playlists />
+                {playlists.length > 0 ?
+                    playlists.map(playlist => <Playlists playlist={playlist} />) :
+                    <h1 className="none-selected">No playlists added currently...</h1>
+                }
             </div>
 
         </div>
