e4aa58e9d0d91f501036361f05b15dca918b5cd3
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-16 23:44:24 +0530

    refactor : had to change some code to make it work after some changes were done to data structure of playlists

--- src/components/Reducer/Reducer.js
+++ src/components/Reducer/Reducer.js
@@ -1,29 +1,30 @@
 import { useReducer, useContext, createContext, useEffect } from 'react'
 import { allVideos } from '../Data/Data'
+import { Playlists } from '../Playlists/Playlists';
 const VideoHandleContext = createContext();
 
 function videosHandler(state, { type, payload }) {
     const { likedVideos, playlists, videos } = state
     switch (type) {
-        case 'INITIAL_VIDEOS':
-            return { ...state, videos: [...videos, payload.videos] }
         case 'ADD_TO_LIKED_VIDEOS':
             return { ...state, likedVideos: [...likedVideos, payload] }
         case 'REMOVE_FROM_LIKED_VIDEOS':
-            return { ...state, likedVideos: likedVideos.filter(video => video.id !== payload.id) }
+            return { ...state, likedVideos: likedVideos.filter(video => video.id !== payload.video.id) }
         case 'ADD_NEW_PLAYLIST':
             return { ...state, playlists: [...playlists, { name: payload.name, list: [] }] }
+        case 'REMOVE_PLAYLIST':
+            return { ...state, playlists: playlists.filter(playlist => playlist.name !== payload.playlist.name) }
         case 'ADD_TO_PLAYLIST':
             const tempPlaylists = [...playlists]
             // index which playlist to mess with
             const index = tempPlaylists.findIndex(loopPlaylist => payload.playlist.name === loopPlaylist.name)
             // update the playlist
-            tempPlaylists[index] = { ...tempPlaylists[index], list: [...tempPlaylists[index].list, payload.videoId] }
+            tempPlaylists[index] = { ...tempPlaylists[index], list: [...tempPlaylists[index].list, payload.video] }
             return { ...state, playlists: tempPlaylists }
         case 'REMOVE_FROM_PLAYLIST':
             const tempPlaylistsRemove = [...playlists]
             const indexToRemoveFrom = tempPlaylistsRemove.findIndex(loopPlaylist => payload.playlist.name === loopPlaylist.name)
-            const filteredVideos = tempPlaylistsRemove[indexToRemoveFrom].list.filter(currentId => currentId !== payload.videoId)
+            const filteredVideos = tempPlaylistsRemove[indexToRemoveFrom].list.filter(currentVideo => currentVideo.id !== payload.video.id)
             tempPlaylistsRemove[indexToRemoveFrom] = { ...tempPlaylistsRemove[indexToRemoveFrom], list: filteredVideos }
             return { ...state, playlists: tempPlaylistsRemove }
 
@@ -34,7 +35,7 @@ export function DataProvider({ children }) {
 
     const likedVideos = []
     const watchLaterVideos = []
-    const playlists = [{ name: 'Add to Watch Later', list: [] }];
+    const playlists = [{ name: 'Watch Later', list: [] }];
     const videos = allVideos
     const [state, dispatch] = useReducer(videosHandler, { likedVideos, watchLaterVideos, playlists, videos })
     return (
--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -1,6 +1,6 @@
 export function checkForIdInPlaylist(playlistList, videoId) {
     console.log("list of videos in our playlist ", playlistList, " and video id being ", videoId)
-    const ifPresent = playlistList.filter(item => item === videoId)
+    const ifPresent = playlistList.filter(item => item.id === videoId)
     console.log("should be ticked or not ", ifPresent.length)
     return ifPresent.length > 0 ? true : false
 }
