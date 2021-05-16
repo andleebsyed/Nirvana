9e77fcfd77c5b3af32481591adedddb7aa7a7dfc
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-16 23:43:20 +0530

    update : added remove button to individual liked videos

--- src/components/Liked/Liked.js
+++ src/components/Liked/Liked.js
@@ -2,12 +2,12 @@ import './Liked.css'
 import { useVideo } from '../Reducer/Reducer'
 import { Card } from '../Card/Card'
 export function Liked() {
-    const { state } = useVideo()
-    const { likedVideos } = state
+    const { state, dispatch } = useVideo()
+    const { likedVideos, playlist } = state
     if (likedVideos.length === 0) {
         return (
             <div className="cards-container">
-                <h1 className="none-liked">No Liked Videos Yet...</h1>
+                <h1 className="none-selected">No Liked Videos Yet...</h1>
             </div>
 
         )
@@ -15,7 +15,16 @@ export function Liked() {
     else {
         return (
             <div className="cards-container">
-                {likedVideos.map(video => <Card video={video} />)}
+                {likedVideos.map(video =>
+                    <div className="cards-container">
+                        <Card video={video} />
+                        <button className=" remove-video-button trash-button" onClick={() => dispatch({ type: 'REMOVE_FROM_LIKED_VIDEOS', payload: { video } })} ><ion-icon name="trash"></ion-icon></button>
+
+
+                    </div>
+
+
+                )}
             </div>
         )
     }
