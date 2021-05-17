75fb5ae70eecec0f0dd4797982a9400b26664b56
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-18 01:56:11 +0530

    update : made categories work

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,18 +1,39 @@
-import './Categories.css'
-import { useVideo } from "../Reducer/Reducer"
+import "./Categories.css";
+import { useVideo } from "../Reducer/Reducer";
+import { useState } from "react";
 
 export function Categories() {
-    const { state, dispatch } = useVideo()
-    const { videos, originalVideos } = state
-    let categories = ["All"]
-    originalVideos.map(video => { if (!categories.includes(video.category)) categories.push(video.category) })
-    return (
-        <div className="buttons-outer" >
-            {categories.map(category =>
-                <button onClick={() => dispatch({ type: 'FILTER_OUT_CATEGORIES', payload: { category } })} className="categories-button">{category}</button>
-            )
-
-            }
-        </div>
-    )
+  const [bgColor, setBgColor] = useState("white");
+  const [clicked, setClicked] = useState(false);
+  const { state, dispatch } = useVideo();
+  const { videos, originalVideos } = state;
+  let categories = ["All"];
+  let classes = "categories-button";
+  function bgColorHandler(category) {
+    console.log("i cam eto change");
+    classes = "categories-button enabled";
+  }
+  originalVideos.map((video) => {
+    if (!categories.includes(video.category)) categories.push(video.category);
+  });
+  return (
+    <div className="buttons-outer">
+      {categories.map((category) => (
+        <>
+          <button
+            onClick={() => (
+              dispatch({
+                type: "FILTER_OUT_CATEGORIES",
+                payload: { category },
+              }),
+              bgColorHandler(category)
+            )}
+            className="categories-button"
+          >
+            {category}
+          </button>
+        </>
+      ))}
+    </div>
+  );
 }
--- src/components/Reducer/Reducer.js
+++ src/components/Reducer/Reducer.js
@@ -1,58 +1,90 @@
-import { useReducer, useContext, createContext, useEffect } from 'react'
-import { allVideos } from '../Data/Data'
-import { Playlists } from '../Playlists/Playlists';
+import { useReducer, useContext, createContext, useEffect } from "react";
+import { allVideos } from "../Data/Data";
+import { Playlists } from "../Playlists/Playlists";
 const VideoHandleContext = createContext();
 
 function videosHandler(state, { type, payload }) {
-    const { likedVideos, playlists, videos, originalVideos } = state
-    // const tempVideos = allVideos
-    switch (type) {
-        case 'ADD_TO_LIKED_VIDEOS':
-            return { ...state, likedVideos: [...likedVideos, payload] }
-        case 'REMOVE_FROM_LIKED_VIDEOS':
-            return { ...state, likedVideos: likedVideos.filter(video => video.id !== payload.video.id) }
-        case 'ADD_NEW_PLAYLIST':
-            return { ...state, playlists: [...playlists, { name: payload.name, list: [] }] }
-        case 'REMOVE_PLAYLIST':
-            return { ...state, playlists: playlists.filter(playlist => playlist.name !== payload.playlist.name) }
-        case 'ADD_TO_PLAYLIST':
-            const tempPlaylists = [...playlists]
-            // index which playlist to mess with
-            const index = tempPlaylists.findIndex(loopPlaylist => payload.playlist.name === loopPlaylist.name)
-            // update the playlist
-            tempPlaylists[index] = { ...tempPlaylists[index], list: [...tempPlaylists[index].list, payload.video] }
-            return { ...state, playlists: tempPlaylists }
-        case 'REMOVE_FROM_PLAYLIST':
-            const tempPlaylistsRemove = [...playlists]
-            const indexToRemoveFrom = tempPlaylistsRemove.findIndex(loopPlaylist => payload.playlist.name === loopPlaylist.name)
-            const filteredVideos = tempPlaylistsRemove[indexToRemoveFrom].list.filter(currentVideo => currentVideo.id !== payload.video.id)
-            tempPlaylistsRemove[indexToRemoveFrom] = { ...tempPlaylistsRemove[indexToRemoveFrom], list: filteredVideos }
-            return { ...state, playlists: tempPlaylistsRemove }
-        case 'FILTER_OUT_CATEGORIES':
-            if (payload.category === 'All') {
-                return { ...state, videos: originalVideos }
-            }
-            else
-                return { ...state, videos: originalVideos.filter(video => video.category === payload.category) }
-
-    }
+  const { likedVideos, playlists, originalVideos } = state;
+  switch (type) {
+    case "ADD_TO_LIKED_VIDEOS":
+      return { ...state, likedVideos: [...likedVideos, payload] };
+    case "REMOVE_FROM_LIKED_VIDEOS":
+      return {
+        ...state,
+        likedVideos: likedVideos.filter(
+          (video) => video.id !== payload.video.id
+        ),
+      };
+    case "ADD_NEW_PLAYLIST":
+      return {
+        ...state,
+        playlists: [...playlists, { name: payload.name, list: [] }],
+      };
+    case "REMOVE_PLAYLIST":
+      return {
+        ...state,
+        playlists: playlists.filter(
+          (playlist) => playlist.name !== payload.playlist.name
+        ),
+      };
+    case "ADD_TO_PLAYLIST":
+      const tempPlaylists = [...playlists];
+      // index which playlist to mess with
+      const index = tempPlaylists.findIndex(
+        (loopPlaylist) => payload.playlist.name === loopPlaylist.name
+      );
+      // update the playlist
+      tempPlaylists[index] = {
+        ...tempPlaylists[index],
+        list: [...tempPlaylists[index].list, payload.video],
+      };
+      return { ...state, playlists: tempPlaylists };
+    case "REMOVE_FROM_PLAYLIST":
+      const tempPlaylistsRemove = [...playlists];
+      const indexToRemoveFrom = tempPlaylistsRemove.findIndex(
+        (loopPlaylist) => payload.playlist.name === loopPlaylist.name
+      );
+      const filteredVideos = tempPlaylistsRemove[indexToRemoveFrom].list.filter(
+        (currentVideo) => currentVideo.id !== payload.video.id
+      );
+      tempPlaylistsRemove[indexToRemoveFrom] = {
+        ...tempPlaylistsRemove[indexToRemoveFrom],
+        list: filteredVideos,
+      };
+      return { ...state, playlists: tempPlaylistsRemove };
+    case "FILTER_OUT_CATEGORIES":
+      if (payload.category === "All") {
+        return { ...state, videos: originalVideos };
+      } else
+        return {
+          ...state,
+          videos: originalVideos.filter(
+            (video) => video.category === payload.category
+          ),
+        };
+  }
 }
 
 export function DataProvider({ children }) {
-
-    const likedVideos = []
-    const watchLaterVideos = []
-    const playlists = [{ name: 'Watch Later', list: [] }];
-    const videos = allVideos
-    const originalVideos = allVideos
-    const [state, dispatch] = useReducer(videosHandler, { likedVideos, watchLaterVideos, playlists, videos, originalVideos })
-    return (
-        <VideoHandleContext.Provider value={{ state, dispatch }}>
-            {children}
-        </VideoHandleContext.Provider>
-    )
+  const likedVideos = [];
+  const watchLaterVideos = [];
+  const playlists = [{ name: "Watch Later", list: [] }];
+  const videos = allVideos;
+  const originalVideos = allVideos;
+  const [state, dispatch] = useReducer(videosHandler, {
+    likedVideos,
+    watchLaterVideos,
+    playlists,
+    videos,
+    originalVideos,
+  });
+  return (
+    <VideoHandleContext.Provider value={{ state, dispatch }}>
+      {children}
+    </VideoHandleContext.Provider>
+  );
 }
 
 export function useVideo() {
-    return useContext(VideoHandleContext);
+  return useContext(VideoHandleContext);
 }
