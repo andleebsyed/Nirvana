fa062c802dc196725f601e4180d9ce55b125b30a
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-19 00:28:32 +0530

    update : call Main component with different data based on current route

--- src/components/Explore/Explore.js
+++ src/components/Explore/Explore.js
@@ -1,11 +1,24 @@
-import './Explore.css'
-import { Categories } from '../Categories/Categories'
-import { Main } from '../Main/Main'
+import "./Explore.css";
+import { Categories } from "../Categories/Categories";
+import { Main } from "../Main/Main";
+import { useVideo } from "../Reducer/Reducer";
+import { useLocation } from "react-router-dom";
 export function Explore() {
-    return (
-        <div>
-            <div className="categories" ><Categories /></div>
-            <div ><Main /></div>
-        </div>
-    )
+  const { state } = useVideo();
+  const { videos, originalVideos } = state;
+  const location = useLocation();
+  const url = location.pathname;
+  console.log("current url ", location.pathname);
+  return (
+    <div>
+      <div className="categories">
+        <Categories />
+      </div>
+      {}
+      <div>
+        {location.pathname === "/explore" && <Main videos={originalVideos} />}
+        {location.pathname === "/categories" && <Main videos={videos} />}
+      </div>
+    </div>
+  );
 }
