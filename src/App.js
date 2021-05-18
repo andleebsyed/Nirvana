560b6bd11a13bd0df188eacf575a611c8ed8e60c
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-18 15:17:41 +0530

    update : changed Search route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,74 @@
-import './App.css';
-import { Header } from './components/Header/Header'
-import { Liked } from './components/Liked/Liked'
-import { Routes, Route } from 'react-router-dom'
-import { VideoPlayer } from './components/VideoPlayer/VideoPlayer';
-import { Explore } from './components/Explore/Explore';
-import { Sidebar } from './components/Sidebar/Sidebar';
-import { Library } from './components/Library/Library';
-import { Homepage } from './components/Homepage/Homepage';
+import "./App.css";
+import { Header } from "./components/Header/Header";
+import { Liked } from "./components/Liked/Liked";
+import { Routes, Route } from "react-router-dom";
+import { VideoPlayer } from "./components/VideoPlayer/VideoPlayer";
+import { Explore } from "./components/Explore/Explore";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import { Library } from "./components/Library/Library";
+import { Homepage } from "./components/Homepage/Homepage";
+import { Search } from "./components/Search/Search";
 function App() {
   return (
     <div className="main-outer-div">
-      <div className="header"><Header /></div>
-      <div className="sidebar"><Sidebar /></div>
+      <div className="header">
+        <Header />
+      </div>
+      <div className="sidebar">
+        <Sidebar />
+      </div>
 
       <Routes>
-        <Route path="/" element={<div className="main"> <Homepage /></div>} />
-        <Route path='/explore' element={<div className="main"><Explore /></div>} />
-        <Route path='/liked' element={<div className="main"><Liked /></div>} />
-        <Route path="watch/:id" element={<div className="main"><VideoPlayer /></div>} />
-        <Route path='library' element={<div className="main"><Library /></div>} />
-
+        <Route
+          path="/"
+          element={
+            <div className="main">
+              {" "}
+              <Homepage />
+            </div>
+          }
+        />
+        <Route
+          path="/explore"
+          element={
+            <div className="main">
+              <Explore />
+            </div>
+          }
+        />
+        <Route
+          path="/liked"
+          element={
+            <div className="main">
+              <Liked />
+            </div>
+          }
+        />
+        <Route
+          path="watch/:id"
+          element={
+            <div className="main">
+              <VideoPlayer />
+            </div>
+          }
+        />
+        <Route
+          path="library"
+          element={
+            <div className="main">
+              <Library />
+            </div>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <div className="main">
+              <Search />
+            </div>
+          }
+        />
       </Routes>
-
     </div>
   );
 }
