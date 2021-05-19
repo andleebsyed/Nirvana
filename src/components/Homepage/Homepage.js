cb376847e9bea9c22f39295e61d1f3b1f361fa4a
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-19 07:25:07 +0530

    update : created basic version of homepage

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,7 +1,19 @@
+import "./Homepage.css";
+import { Link } from "react-router-dom";
 export function Homepage() {
-    return (
-        <div>
-            <h1>homepage</h1>
-        </div>
-    )
+  return (
+    <div className="homepage">
+      <p>Life is a trip and travelling is living twice...</p>
+      <p>So why not dare to live twice and get out there to feed your soul</p>
+      <p>Warning : Content may cause wanderlust</p>
+      <div className="homepage-buttons-outer">
+        <Link to="/explore">
+          <button className="homepage-single-button">Explore</button>
+        </Link>
+        <Link to="#">
+          <button className="homepage-single-button">Login</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
