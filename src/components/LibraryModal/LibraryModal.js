c33bf19032c630bef156ddb85ae4cef321ab80c8
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-20 01:24:24 +0530

    update : added a model to pop up when video is added/removed to/from a specific playlist

--- src/components/LibraryModal/LibraryModal.js
+++ src/components/LibraryModal/LibraryModal.js
@@ -1,68 +1,90 @@
-import './LibraryModal.css'
-import { useState, useRef } from 'react'
-import { checkForIdInPlaylist } from '../../utils/funcs'
-import { useVideo } from '../Reducer/Reducer'
+import "./LibraryModal.css";
+import { useState, useRef } from "react";
+import { checkForIdInPlaylist } from "../../utils/funcs";
+import { useVideo } from "../Reducer/Reducer";
 export function LibraryModal({ show, setShow, video }) {
-    const { state, dispatch } = useVideo()
-    const { playlists } = state
-    const [current, setCurrent] = useState('')
-    const inputEl = useRef(null)
+  const { state, dispatch } = useVideo();
+  const { playlists } = state;
+  const [current, setCurrent] = useState("");
+  const inputEl = useRef(null);
+  const modalClass = {
+    offclick: "modale",
+    onclick: "modale active",
+  };
+  const [showModal, setShowModal] = useState(false);
+  const [modalText, setModalText] = useState("");
 
-    function keyPressHandler(e) {
-        return e.key === 'Enter' ? (
-            dispatch({ type: 'ADD_NEW_PLAYLIST', payload: { name: current } }), e.currentTarget.value = "") : 'do nothing'
+  function keyPressHandler(e) {
+    return e.key === "Enter"
+      ? (dispatch({ type: "ADD_NEW_PLAYLIST", payload: { name: current } }),
+        (e.currentTarget.value = ""))
+      : "do nothing";
+  }
+
+  function onClickHandler(e) {
+    dispatch({ type: "ADD_NEW_PLAYLIST", payload: { name: current } });
+    inputEl.current.value = "";
+  }
+  function checkboxHandler(playlist, video) {
+    if (checkForIdInPlaylist(playlist.list, video.id) === true) {
+      dispatch({ type: "REMOVE_FROM_PLAYLIST", payload: { playlist, video } });
+      setModalText(`Removed from ${playlist.name}`);
+      setShowModal(true);
+      setTimeout(() => setShowModal(false), 1300);
+    } else {
+      dispatch({ type: "ADD_TO_PLAYLIST", payload: { playlist, video } });
+
+      setModalText(`Added to  ${playlist.name}`);
+      setShowModal(true);
+      setTimeout(() => setShowModal(false), 1300);
     }
+  }
 
-    function onClickHandler(e) {
-        dispatch({ type: 'ADD_NEW_PLAYLIST', payload: { name: current } })
-        inputEl.current.value = ""
-
-    }
-    function checkboxHandler(playlist, video) {
-        if (checkForIdInPlaylist(playlist.list, video.id) === true) {
-
-            dispatch({ type: 'REMOVE_FROM_PLAYLIST', payload: { playlist, video } })
-        }
-        else {
-            dispatch({ type: 'ADD_TO_PLAYLIST', payload: { playlist, video } })
-        }
-    }
-
-
-    if (!show) {
-        return null
-    }
-    else {
-        console.log("playlist is being populated or not ", playlists)
-
-        return (
-            <div className="modal-outer" onClick={() => setShow(false)}>
-                <div className="modal" onClick={(e) => e.stopPropagation()}>
-                    <div className="modal-header">
-                        <p>Add to Library</p>
-                        <button className="close-modal" onClick={() => setShow(false)}>X</button>
-                    </div>
-                    <div className="playlists">
-
-                        {playlists.map(playlist =>
-                            <div className="single-playlist">
-                                <input type="checkbox" checked={checkForIdInPlaylist(playlist.list, video.id)} onChange={() => checkboxHandler(playlist, video)} />
-                                <label>{playlist.name}</label>
-                                {/* <button className="delete-playlist-button" >X</button> */}
-                            </div>
-                        )}
-
-                    </div>
-                    <div className="modal-footer">
-                        <input type="text" id="input" ref={inputEl} placeholder="Add new..." onChange={(e) => setCurrent(e.target.value)} onKeyPress={(e) => keyPressHandler(e)} />
-                        <button className="footer-button" onClick={(e) => onClickHandler(e)}>Add</button>
-                    </div>
-
-
-                </div>
-
-            </div >
-        )
-    }
-
+  if (!show) {
+    return null;
+  } else {
+    return (
+      <div className="modal-outer" onClick={() => setShow(false)}>
+        <div className="modal" onClick={(e) => e.stopPropagation()}>
+          <div className="modal-header">
+            <p>Add to Library</p>
+            <button className="close-modal" onClick={() => setShow(false)}>
+              X
+            </button>
+          </div>
+          <div className="playlists">
+            {playlists.map((playlist) => (
+              <div className="single-playlist">
+                <input
+                  type="checkbox"
+                  checked={checkForIdInPlaylist(playlist.list, video.id)}
+                  onChange={() => checkboxHandler(playlist, video)}
+                />
+                <label>{playlist.name}</label>
+              </div>
+            ))}
+          </div>
+          <div className="modal-footer">
+            <input
+              type="text"
+              id="input"
+              ref={inputEl}
+              placeholder="Add new..."
+              onChange={(e) => setCurrent(e.target.value)}
+              onKeyPress={(e) => keyPressHandler(e)}
+            />
+            <button
+              className="footer-button"
+              onClick={(e) => onClickHandler(e)}
+            >
+              Add
+            </button>
+          </div>
+        </div>
+        <div className={showModal ? modalClass.onclick : modalClass.offclick}>
+          {modalText}
+        </div>
+      </div>
+    );
+  }
 }
