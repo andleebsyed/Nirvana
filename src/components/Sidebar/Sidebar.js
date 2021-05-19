4011f1d9c085739e79e5e45e3de17efe7c3d9f66
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-19 08:56:41 +0530

    update : used NavLink on Sidebar links to highlight them when active

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,29 +1,45 @@
-import './Sidebar.css'
-import { Link } from 'react-router-dom'
+import "./Sidebar.css";
+import { NavLink } from "react-router-dom";
 export function Sidebar() {
-    return (
-        <div className="sidebar">
-            <Link to='/' className="sidebar-link">
-                <div className="side-icon">
-                    <a className="adjustment"><ion-icon name="home-outline"></ion-icon></a><span className="adjustment icon-text">Home</span>
-
-                </div>
-            </Link>
-            <Link to='/explore' className="sidebar-link">
-                <div className="side-icon">
-                    <a className="adjustment"><ion-icon name="compass-outline"></ion-icon></a><span className="adjustment icon-text">Explore</span>
-
-                </div>
-            </Link>
-            <Link to='/library' className="sidebar-link">
-                <div className="side-icon">
-                    <a className="adjustment"><ion-icon name="library-outline"></ion-icon></a><span className="adjustment icon-text">Library</span>
-
-                </div>
-            </Link>
-
+  return (
+    <div className="sidebar">
+      <NavLink
+        to="/"
+        end
+        className="sidebar-link"
+        activeClassName="sidebar-icon-selected"
+      >
+        <div className="side-icon">
+          <a className="adjustment">
+            <ion-icon name="home-outline"></ion-icon>
+          </a>
+          <span className="adjustment icon-text">Home</span>
         </div>
-    )
+      </NavLink>
+      <NavLink
+        to="/explore"
+        className="sidebar-link"
+        activeClassName="sidebar-icon-selected"
+      >
+        <div className="side-icon">
+          <a className="adjustment">
+            <ion-icon name="compass-outline"></ion-icon>
+          </a>
+          <span className="adjustment icon-text">Explore</span>
+        </div>
+      </NavLink>
+      <NavLink
+        to="/library"
+        className="sidebar-link"
+        activeClassName="sidebar-icon-selected"
+      >
+        <div className="side-icon">
+          <a className="adjustment">
+            <ion-icon name="library-outline"></ion-icon>
+          </a>
+          <span className="adjustment icon-text">Library</span>
+        </div>
+      </NavLink>
+    </div>
+  );
 }
-
-
