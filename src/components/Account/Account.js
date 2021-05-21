84d825a0761d9af444f50cb227f51ca63085c59a
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-21 17:06:55 +0530

    update : made logout button work

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,12 +1,16 @@
 import "./Account.css";
 import { ProfileDetails } from "../ProfileDetails/ProfileDetails";
 import { PasswordReset } from "../PasswordReset/PasswordReset";
+import { useVideo } from "../Reducer/Reducer";
+import { useAuth } from "../Reducer/AuthReducer";
 export function Account() {
+  const { dispatchAuth } = useAuth();
   return (
     <div className="outer-div">
       <div className="heading">
         <h1 className="welcome-text">user</h1>
         <button
+          onClick={() => dispatchAuth({ type: "LOGOUT_USER" })}
           className="logout-button-custom"
           //   onClick={handleLogout}
         >
