57246b71597a41b64ec644ff4b65e181657c30d9
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-27 18:19:46 +0530

    fix : changed some variable names as conflict was rising with back end

--- src/components/Playlists/Playlists.js
+++ src/components/Playlists/Playlists.js
@@ -1,52 +1,60 @@
-import './Playlists.css'
-import { Card } from "../Card/Card"
-import { useVideo } from '../Reducer/Reducer'
+import "./Playlists.css";
+import { Card } from "../Card/Card";
+import { useVideo } from "../Reducer/Reducer";
 
 export function Playlists({ playlist }) {
-    const { dispatch, state } = useVideo()
-    const { playlists } = state
-    if (playlists.length > 0) {
-        return (
-            <>
-                {playlists.map(playlist =>
-                    <>
-                        <div className="single-playlist-show">
-                            <h1 style={{ fontWeight: 'lighter', textAlign: 'left' }} >{playlist.name}</h1>
-                            <button onClick={() => dispatch({ type: 'REMOVE_PLAYLIST', payload: { playlist } })} className="trash-button"><ion-icon name="trash"></ion-icon></button>
-                        </div>
-                        {playlist.list.length > 0 ?
-                            <div className="video-container">
-                                {playlist.list.map(video =>
-                                    <div className="video-container">
-                                        <Card video={video} />
-                                        <button className=" remove-video-button trash-button" onClick={() => dispatch({ type: 'REMOVE_FROM_PLAYLIST', payload: { playlist, video } })} ><ion-icon name="trash"></ion-icon></button>
-                                    </div>
-
-                                )}
-                            </div>
-                            :
-                            <div>
-                                <h1 className="none-selected">No videos in here currently...</h1>
-
-                            </div>
-
-                        }
-
-
-                    </>
-
-                )}
-            </>
-
-
-
-        )
-
-    }
-    else {
-        return (
-            <h1 className="none-selected">No playlists added currently...</h1>
-        )
-    }
-
+  const { dispatch, state } = useVideo();
+  const { playlists } = state;
+  console.log("my user's playlists ", playlists);
+  if (playlists.length > 0) {
+    return (
+      <>
+        {playlists.map((playlist) => (
+          <>
+            <div className="single-playlist-show">
+              <h1 style={{ fontWeight: "lighter", textAlign: "left" }}>
+                {playlist.playlistName}
+              </h1>
+              <button
+                onClick={() =>
+                  dispatch({ type: "REMOVE_PLAYLIST", payload: { playlist } })
+                }
+                className="trash-button"
+              >
+                <ion-icon name="trash"></ion-icon>
+              </button>
+            </div>
+            {playlist.videos.length > 0 ? (
+              <div className="video-container">
+                {playlist.videos.map((video) => (
+                  <div className="video-container">
+                    <Card video={video} />
+                    <button
+                      className=" remove-video-button trash-button"
+                      onClick={() =>
+                        dispatch({
+                          type: "REMOVE_FROM_PLAYLIST",
+                          payload: { playlist, video },
+                        })
+                      }
+                    >
+                      <ion-icon name="trash"></ion-icon>
+                    </button>
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div>
+                <h1 className="none-selected">
+                  No videos in here currently...
+                </h1>
+              </div>
+            )}
+          </>
+        ))}
+      </>
+    );
+  } else {
+    return <h1 className="none-selected">No playlists added currently...</h1>;
+  }
 }
