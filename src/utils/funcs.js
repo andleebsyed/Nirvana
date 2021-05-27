20fb373b0f70593fb2e0310e747d35b97c05cdcd
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-27 18:25:44 +0530

    refactor : refactor by prettier

--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -1,6 +1,11 @@
-export function checkForIdInPlaylist(playlistList, videoId) {
-    console.log("list of videos in our playlist ", playlistList, " and video id being ", videoId)
-    const ifPresent = playlistList.filter(item => item.id === videoId)
-    console.log("should be ticked or not ", ifPresent.length)
-    return ifPresent.length > 0 ? true : false
+export function checkForIdInPlaylist(playlist, videoId) {
+  console.log(
+    "list of videos in our playlist ",
+    playlist,
+    " and video id being ",
+    videoId
+  );
+  const ifPresent = playlist.filter((item) => item.id === videoId);
+  console.log("should be ticked or not ", ifPresent.length);
+  return ifPresent.length > 0 ? true : false;
 }
