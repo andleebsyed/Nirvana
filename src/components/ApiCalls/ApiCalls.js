4ee25d9a7bee1357faaed9cc13162c763a295629
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-27 14:16:58 +0530

    update : made some async function which will be fired on particular action by user on view like add video to liked videos , remove video from liked videos etc

--- src/components/ApiCalls/ApiCalls.js
+++ src/components/ApiCalls/ApiCalls.js
@@ -12,9 +12,11 @@ export async function UserSignIn(username, password) {
       dataFromView
     );
     if (response.status === 200) {
+      console.log("data in response ", response.data);
       const userResponseFromServer = {
         allowUser: response.data.allowUser,
         messageToShowOnView: response.data.message,
+        userId: response.data.user?._id,
       };
       return userResponseFromServer;
     }
@@ -33,11 +35,13 @@ export async function UserSignUp(userDetails) {
       "https://video-library-api.andydev7.repl.co/users/signup",
       signUpDataFromView
     );
+    console.log("resposne on signup ", response);
     if (response.status === 200) {
       if (response.data.status === true) {
+        console.log("resposne on signup ", response);
         return (isSignUpSuccessfull = {
           status: true,
-          username: userDetails.username,
+          userId: response.data.user._id,
         });
       } else if (response.data.status === false) {
         //  11000 is returned when we send data which is already there for common field
@@ -65,5 +69,64 @@ export async function GetVideos() {
     }
   } catch (error) {
     console.log("error occurred ", error);
+  }
+}
+
+export async function GetLikedVideos() {
+  const userId = localStorage.getItem("userId");
+
+  try {
+    const data = { UserId: userId };
+    const response = await axios.post(
+      "https://video-library-api.andydev7.repl.co/liked/all",
+      data
+    );
+    console.log("response on fetchng liked videos ", response);
+    if (response.status === 200) {
+      return response.data.likedVideos;
+    }
+  } catch (error) {
+    console.log("Oops!an error occurred ", error.response.data);
+  }
+}
+
+export async function SaveToLiked(dispatch, video) {
+  try {
+    const userId = localStorage.getItem("userId");
+    const data = { userId: userId, videoId: video._id };
+    const response = await axios.post(
+      "https://video-library-api.andydev7.repl.co/liked/add",
+      data
+    );
+    console.log("rsponse on add to like ", response);
+    if (response.status === 200) {
+      dispatch({
+        type: "ADD_TO_LIKED_VIDEOS",
+        payload: { video },
+      });
+    }
+  } catch (error) {
+    console.log("ean error occurred ", error);
+  }
+}
+
+export async function RemoveFromLikedVideos(dispatch, video, userId) {
+  console.log(" i am called to remove video from liked videos");
+  try {
+    // const userId = localStorage.getItem("userId");
+    const data = { videoId: video._id, userId: userId };
+    const response = await axios.post(
+      "https://video-library-api.andydev7.repl.co/liked/delete",
+      data
+    );
+    console.log("response from server is ", response);
+    if (response.status === 200) {
+      dispatch({
+        type: "REMOVE_FROM_LIKED_VIDEOS",
+        payload: { video },
+      });
+    }
+  } catch (error) {
+    console.log("error occured ", error.response.data);
   }
 }
