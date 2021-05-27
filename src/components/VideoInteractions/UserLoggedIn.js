3bcdc2cf79c653cad43eb9fc6584db2959d952b5
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-27 14:25:02 +0530

    update : on user logged in added backend updation property when doing like/unlike on a video in addition with updating state on frontend(all happening in function called in each use case)

--- src/components/VideoInteractions/UserLoggedIn.js
+++ src/components/VideoInteractions/UserLoggedIn.js
@@ -1,31 +1,45 @@
 import { useVideo } from "../Reducer/Reducer";
+import {
+  GetLikedVideos,
+  SaveToLiked,
+  RemoveFromLikedVideos,
+} from "../ApiCalls/ApiCalls";
 // css from VideoPlayes.css
 
 export function UserLoggedIn({ videoInLiked, video, setShow }) {
   const { dispatch } = useVideo();
+  console.log("id of video", video._id);
+  const userId = localStorage.getItem("userId");
+  console.log("usr id is ", userId);
+  // async function addtoLiked(video) {
+  //   dispatch({
+  //     type: "ADD_TO_LIKED_VIDEOS",
+  //     payload: { video },
+  //   }),
+  //     await GetLikedVideos();
+  // }
+
   return (
     <div className="interactions">
       {videoInLiked.length === 0 ? (
         <button
           title="Like"
-          onClick={() => {
-            dispatch({
-              type: "ADD_TO_LIKED_VIDEOS",
-              payload: { video },
-            });
-          }}
+          onClick={() => SaveToLiked(dispatch, video)}
           className="not-liked buttons"
         >
           <ion-icon name="thumbs-up-outline"></ion-icon>
         </button>
       ) : (
         <button
-          onClick={() => {
-            dispatch({
-              type: "REMOVE_FROM_LIKED_VIDEOS",
-              payload: { video },
-            });
-          }}
+          onClick={
+            () => RemoveFromLikedVideos(dispatch, video, userId)
+            //   () => {
+            // dispatch({
+            //   type: "REMOVE_FROM_LIKED_VIDEOS",
+            //   payload: { video },
+            // });
+            // }
+          }
           className="liked buttons"
         >
           <ion-icon name="thumbs-up-sharp"></ion-icon>
