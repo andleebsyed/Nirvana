47885ce3be6491b19b68b38ff6cf9a0915ec344e
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-27 17:36:31 +0530

    refactor : removed unused code

--- src/components/VideoInteractions/UserLoggedIn.js
+++ src/components/VideoInteractions/UserLoggedIn.js
@@ -8,38 +8,20 @@ import {
 
 export function UserLoggedIn({ videoInLiked, video, setShow }) {
   const { dispatch } = useVideo();
-  console.log("id of video", video._id);
   const userId = localStorage.getItem("userId");
-  console.log("usr id is ", userId);
-  // async function addtoLiked(video) {
-  //   dispatch({
-  //     type: "ADD_TO_LIKED_VIDEOS",
-  //     payload: { video },
-  //   }),
-  //     await GetLikedVideos();
-  // }
-
   return (
     <div className="interactions">
       {videoInLiked.length === 0 ? (
         <button
           title="Like"
-          onClick={() => SaveToLiked(dispatch, video)}
+          onClick={() => SaveToLiked(dispatch, video, userId)}
           className="not-liked buttons"
         >
           <ion-icon name="thumbs-up-outline"></ion-icon>
         </button>
       ) : (
         <button
-          onClick={
-            () => RemoveFromLikedVideos(dispatch, video, userId)
-            //   () => {
-            // dispatch({
-            //   type: "REMOVE_FROM_LIKED_VIDEOS",
-            //   payload: { video },
-            // });
-            // }
-          }
+          onClick={() => RemoveFromLikedVideos(dispatch, video, userId)}
           className="liked buttons"
         >
           <ion-icon name="thumbs-up-sharp"></ion-icon>
