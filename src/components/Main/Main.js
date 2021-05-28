07e94a96c87372bc33d5c52dda0ed1b6d8d94cb9
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-28 17:39:44 +0530

    style : did some tinkering in  placement of some buttons on ui

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,7 @@ import "./Main.css";
 import { Card } from "../Card/Card";
 export function Main({ videos }) {
   return (
-    <div className="main-cards-container abc">
+    <div className="main-cards-container cards-margin">
       {videos.map((video) => (
         <Card video={video} />
       ))}
