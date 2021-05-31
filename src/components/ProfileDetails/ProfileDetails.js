dccf27740dd25a0d77b84e028bb539d48b5bebd8
Syed Andleeb <andleebsyed75@gmail.com>
2021-05-31 08:29:10 +0530

    update : handled the response on request of user update, and give message to user accordingly

--- src/components/ProfileDetails/ProfileDetails.js
+++ src/components/ProfileDetails/ProfileDetails.js
@@ -1,12 +1,19 @@
 import "./ProfileDetails.css";
 import { useEffect, useState, useRef } from "react";
 import { useAuth } from "../Reducer/AuthReducer";
-import { GetAccountDetails } from "../ApiCalls/ApiCalls";
+import { GetAccountDetails, UpdateUserDetails } from "../ApiCalls/ApiCalls";
 export function ProfileDetails() {
   const { stateAuth } = useAuth();
   const { isUserAuthenticated } = stateAuth;
   const usernameEl = useRef(null);
   const emailEl = useRef(null);
+  const [newUsername, setNewUsername] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [updateMessage, setUpdateMessage] = useState({
+    message: "a",
+    styleClass: "update-inital-render-class",
+  });
+  console.log("state for update ", updateMessage);
   useEffect(() => {
     async function ApiCall() {
       if (isUserAuthenticated) {
@@ -14,22 +21,41 @@ export function ProfileDetails() {
         if (usernameEl.current !== null || undefined) {
           usernameEl.current.value = response.username;
           emailEl.current.value = response.email;
+          setNewUsername(response.username);
+          setNewEmail(response.email);
         }
       }
     }
     ApiCall();
   }, []);
+  async function AccountUpdateHandler(e) {
+    e.preventDefault();
+    console.log("i came in form submit");
+
+    const response = await UpdateUserDetails(newUsername, newEmail);
+    console.log("user respo ", response);
+    response.status === true
+      ? setUpdateMessage({
+          message: response.message,
+          styleClass: "update-status update-success",
+        })
+      : setUpdateMessage({
+          message: response.message,
+          styleClass: "update-status update-failure",
+        });
+  }
   return (
-    <form>
+    <form onSubmit={AccountUpdateHandler}>
       <div className="profile-details width-adjust">
         <p className="label">Account</p>
+        <p className={updateMessage.styleClass}>{updateMessage.message}</p>
         <div className="holder">
           <label className="labels-acc" for="username">
             Username
           </label>
           <input
             ref={usernameEl}
-            type="text"
+            onChange={(e) => setNewUsername(e.target.value)}
             name="username"
             type="username"
             className="input-box acc-username"
@@ -43,7 +69,8 @@ export function ProfileDetails() {
           </label>
           <input
             ref={emailEl}
-            type="text"
+            onChange={(e) => setNewEmail(e.target.value)}
+            // type="text"
             name="email"
             type="email"
             className="input-box acc-password"
