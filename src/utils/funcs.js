be06badb9690c9f0447c00222fa1ad8a344d553a
Syed Andleeb <andleebsyed75@gmail.com>
2021-06-03 12:01:56 +0530

    update : added a function to set up Authorization header for login/logout cases

--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 export function checkForIdInPlaylist(playlist, videoId) {
   console.log(
     "list of videos in our playlist ",
@@ -7,4 +8,17 @@ export function checkForIdInPlaylist(playlist, videoId) {
   );
   const ifPresent = playlist.filter((item) => item.id === videoId);
   return ifPresent.length > 0 ? true : false;
+}
+
+export function setupAuthHeaderForServiceCalls(token) {
+  if (token) {
+    console.log(
+      "i am called with token ",
+      { token },
+      "and now after this eacgh rewuet will have token"
+    );
+    return (axios.defaults.headers.common["Authorization"] = token);
+  }
+  console.log("token nahi aaraha delete krna padega");
+  delete axios.defaults.headers.common["Authorization"];
 }
