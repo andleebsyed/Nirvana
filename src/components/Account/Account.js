525bce27f18c9fc7a19520a290572647292bd654
Syed Andleeb <andleebsyed75@gmail.com>
2021-06-05 15:23:47 +0530

    update : added loader to account page

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -4,16 +4,21 @@ import { PasswordReset } from "../PasswordReset/PasswordReset";
 import { useVideo } from "../Reducer/Reducer";
 import { useAuth } from "../Reducer/AuthReducer";
 import { useState } from "react";
+import { SetLoader } from "../Loader/Loader";
 export function Account() {
   const { dispatchAuth } = useAuth();
   const { dispatch } = useVideo();
-  const [username, setUsername] = useState("...");
+  const [username, setUsername] = useState(null);
+
   // passed to ProfilrDetails to get username from there and populate username on this page
   function getUser(comingUsername) {
     setUsername(comingUsername);
   }
   return (
     <div className="outer-div">
+      {/* until username is fetched show loader */}
+      {!username ? <SetLoader /> : ""}
+
       <div className="heading">
         <h1 className="welcome-text">{username}</h1>
         <button
@@ -26,6 +31,7 @@ export function Account() {
           Logout
         </button>
       </div>
+
       <div className="cards-container">
         <ProfileDetails props={{ getUser }} />
         <PasswordReset />
