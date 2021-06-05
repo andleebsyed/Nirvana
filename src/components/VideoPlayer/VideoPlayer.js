c51dbf7d4577265c7682bb867cd0f5313d0b01ce
Syed Andleeb <andleebsyed75@gmail.com>
2021-06-05 11:45:18 +0530

    update : made videoplayer direct link work by adding a loader

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -8,9 +8,9 @@ import { Notes } from "../Notes/Notes";
 import { useAuth } from "../Reducer/AuthReducer";
 import { UserNotLoggedIn } from "../VideoInteractions/UserNotLoggedIn";
 import { UserLoggedIn } from "../VideoInteractions/UserLoggedIn";
-import { GetVideos } from "../ApiCalls/ApiCalls";
+// import { GetVideos } from "../ApiCalls/ApiCalls";
 export function VideoPlayer() {
-  const { dispatch } = useVideo();
+  // const { dispatch } = useVideo();
   const { state } = useVideo();
   const { originalVideos, likedVideos } = state;
   const { stateAuth } = useAuth();
@@ -25,50 +25,58 @@ export function VideoPlayer() {
 
   // to be passed to Library Modal
   let props = { show: show, setShow: setShow, video: video };
-  return (
-    <div className="outer-main">
-      <div className="player-card">
-        <div className="player">
-          <ReactPlayer
-            url={video.url}
-            controls={true}
-            width={"55vw"}
-            height={"60vh"}
-          />
-        </div>
-        <div className="video-label">
-          <p className="video-intro">{video.videoName}</p>
-
-          <div className="interactions">
-            {isUserAuthenticated ? (
-              <UserLoggedIn
-                videoInLiked={videoInLiked}
-                video={video}
-                setShow={setShow}
-              />
-            ) : (
-              <UserNotLoggedIn />
-            )}
-          </div>
-        </div>
-        <div className="creator-details">
-          <div className="thumbnail-and-name">
-            <img
-              className="thumbnail"
-              src={video.creatorThumbnail}
-              alt="creator-thumbnail"
+  if (video) {
+    return (
+      <div className="outer-main">
+        <div className="player-card">
+          <div className="player">
+            <ReactPlayer
+              url={video.url}
+              controls={true}
+              width={"55vw"}
+              height={"60vh"}
             />
-            <p className="name">{video.creatorName}</p>
           </div>
-          <div className="video-details">
-            <p className="video-description">{video.description}</p>
+          <div className="video-label">
+            <p className="video-intro">{video.videoName}</p>
+
+            <div className="interactions">
+              {isUserAuthenticated ? (
+                <UserLoggedIn
+                  videoInLiked={videoInLiked}
+                  video={video}
+                  setShow={setShow}
+                />
+              ) : (
+                <UserNotLoggedIn />
+              )}
+            </div>
+          </div>
+          <div className="creator-details">
+            <div className="thumbnail-and-name">
+              <img
+                className="thumbnail"
+                src={video.creatorThumbnail}
+                alt="creator-thumbnail"
+              />
+              <p className="name">{video.creatorName}</p>
+            </div>
+            <div className="video-details">
+              <p className="video-description">{video.description}</p>
+            </div>
           </div>
         </div>
+
+        <Notes video={video} />
+
+        <LibraryModal {...props} />
       </div>
-
-      <Notes video={video} />
-
-      <LibraryModal {...props} />
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div>
+        <h1>loading</h1>
+      </div>
+    );
+  }
 }
