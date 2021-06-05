b442f530e7120481a039b10c37a105ac82482775
Syed Andleeb <andleebsyed75@gmail.com>
2021-06-05 15:48:09 +0530

    update : added loader to Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -3,10 +3,11 @@ import { useLocation, Link } from "react-router-dom";
 import { useVideo } from "../Reducer/Reducer";
 import { useEffect } from "react";
 import { Main } from "../Main/Main";
+import { SetLoader } from "../Loader/Loader";
 
 export function Search() {
   const { dispatch, state } = useVideo();
-  const { videos } = state;
+  const { videos, originalVideos } = state;
   //   to get the query params we use useLocation() from react-router
   function useQuery() {
     //   constructor accepts query string | returns URLSearchParms() object
@@ -21,21 +22,26 @@ export function Search() {
       type: "SEARCH_FOR_VIDEOS",
       payload: { searchKeyword: keyword },
     });
-  }, []);
-
-  return (
-    <div>
-      {videos.length > 0 ? (
-        <Main videos={videos} />
-      ) : (
-        <div className="none-found">
-          <h1 className="heading-intro">Oops!No Videos matched your keyword</h1>
-          <Link to="/explore">
-            <button className="button-back">Explore</button>
-          </Link>
+  }, [originalVideos]);
+  if (originalVideos.length > 0) {
+    return (
+      <div>
+        {videos.length > 0 ? (
           <Main videos={videos} />
-        </div>
-      )}
-    </div>
-  );
+        ) : (
+          <div className="none-found">
+            <h1 className="heading-intro">
+              Oops!No Videos matched your keyword
+            </h1>
+            <Link to="/explore">
+              <button className="button-back">Explore</button>
+            </Link>
+            {/* <Main videos={videos} /> */}
+          </div>
+        )}
+      </div>
+    );
+  } else {
+    return <SetLoader />;
+  }
 }
