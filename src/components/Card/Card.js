a8297257da881f44dfe2db6192c400bed3e188bd
Syed Andleeb <andleebsyed75@gmail.com>
2021-06-06 11:52:03 +0530

    style : fixed positioning bug of cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,20 @@
-import './Card.css'
-import { Link } from 'react-router-dom'
+import "./Card.css";
+import { Link } from "react-router-dom";
 export function Card({ video }) {
-    return (
-        <Link to={`/watch/${video.id}`
-        } className="video-card">
-            <img className="card-image" src={video.thumbnail} />
-            <p className="video-name">{video.videoName}</p>
-            <img className="card-badge" src={video.creatorThumbnail} />
-            <p className="creator-name">{video.creatorName}</p>
-
-        </Link>
-    )
+  return (
+    <Link to={`/watch/${video.id}`} className="video-card">
+      <img className="card-image" src={video.thumbnail} alt="video-thumbmail" />
+      <p className="video-name">{video.videoName}</p>
+      <div className="creator-details-outer">
+        <img
+          className="card-badge"
+          src={video.creatorThumbnail}
+          alt="creator-thumbnail"
+        />
+        {/* <div className="creator-name"> */}
+        <p className="creator-name">{video.creatorName}</p>
+      </div>
+      {/* </div> */}
+    </Link>
+  );
 }
