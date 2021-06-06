78217e490ee1ee286fccef6550b7ff3853608a29
Syed Andleeb <andleebsyed75@gmail.com>
2021-06-06 20:56:08 +0530

    update : added loader to Liked Component to show when video is being deleted

--- src/components/Liked/Liked.js
+++ src/components/Liked/Liked.js
@@ -2,9 +2,12 @@ import "./Liked.css";
 import { useVideo } from "../Reducer/Reducer";
 import { Card } from "../Card/Card";
 import { RemoveFromLikedVideos } from "../ApiCalls/ApiCalls";
+import { SetLoader } from "../Loader/Loader";
+import { useState } from "react";
 export function Liked() {
   const { state, dispatch } = useVideo();
   const { likedVideos, playlist } = state;
+  const [isLoading, setIsLoading] = useState(false);
   const userId = localStorage.getItem("userId");
   if (likedVideos.length === 0) {
     return (
@@ -20,14 +23,21 @@ export function Liked() {
             <Card video={video} />
             <button
               className=" remove-video-button trash-button"
-              onClick={async () =>
-                await RemoveFromLikedVideos(dispatch, video, userId)
-              }
+              onClick={async () => {
+                setIsLoading(true);
+                await RemoveFromLikedVideos(dispatch, video, userId);
+                setIsLoading(false);
+              }}
             >
               <ion-icon name="trash"></ion-icon>
             </button>
           </div>
         ))}
+        {isLoading && (
+          <div className="remove-like-loader">
+            <SetLoader />
+          </div>
+        )}
       </div>
     );
   }
