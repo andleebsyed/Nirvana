31011a987310d7f99757c875a38bd6a4d7b5936e
Syed Andleeb <andleebsyed75@gmail.com>
2021-06-07 11:10:32 +0530

    update : added loader on action of removing video from playlist/removing playlist

--- src/components/Playlists/Playlists.js
+++ src/components/Playlists/Playlists.js
@@ -1,11 +1,14 @@
 import "./Playlists.css";
+import { useState } from "react";
 import { Card } from "../Card/Card";
 import { useVideo } from "../Reducer/Reducer";
 import { DeleteFromPlaylist, RemovePlaylist } from "../ApiCalls/ApiCalls";
+import { SetLoader } from "../Loader/Loader";
 
 export function Playlists({ playlist }) {
   const { dispatch, state } = useVideo();
   const { playlists } = state;
+  const [isLoading, setIsLoading] = useState(false);
   if (playlists.length > 0) {
     return (
       <>
@@ -16,13 +19,27 @@ export function Playlists({ playlist }) {
                 {playlist.playlistName}
               </h1>
               <button
-                onClick={async () =>
-                  await RemovePlaylist(playlist._id, dispatch)
-                }
+                onClick={async () => {
+                  setIsLoading({
+                    status: true,
+                    playlistName: playlist.playlistName,
+                  });
+                  await RemovePlaylist(playlist._id, dispatch);
+                  setIsLoading({
+                    status: false,
+                    playlistName: playlist.playlistName,
+                  });
+                }}
                 className="trash-button"
               >
                 <ion-icon name="trash"></ion-icon>
               </button>
+              {isLoading.status &&
+                isLoading.playlistName === playlist.playlistName && (
+                  <div className="playlist-loader">
+                    <SetLoader />
+                  </div>
+                )}
             </div>
             {playlist.videos.length > 0 ? (
               <div className="video-container">
@@ -32,11 +49,16 @@ export function Playlists({ playlist }) {
                     <button
                       className=" remove-video-button trash-button"
                       onClick={async () => {
+                        setIsLoading({
+                          status: true,
+                          playlistName: playlist.playlistName,
+                        });
                         await DeleteFromPlaylist(
                           video._id,
                           playlist._id,
                           dispatch
                         );
+                        setIsLoading(false);
                         //   setModalText(`Removed video from ${playlist.playlistName}`);
                         //   setShowModal(true);
                         //   setTimeout(() => setShowModal(false), 1300);
