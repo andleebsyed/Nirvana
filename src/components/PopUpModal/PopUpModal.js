2d8fc6d8ff73cc79442494f527836b3ce2961336
Syed Andleeb <andleebsyed75@gmail.com>
2021-06-09 12:20:11 +0530

    refactor : removed unused code

--- src/components/PopUpModal/PopUpModal.js
+++ src/components/PopUpModal/PopUpModal.js
@@ -1,7 +1,6 @@
 import "./PopUpModal.css";
 export function PopUpModal({ props }) {
   const { showModal, modalText } = props;
-  console.log("props we are etting are", { props });
   return (
     <div className={showModal ? "modale active" : "modale"}>{modalText}</div>
   );
