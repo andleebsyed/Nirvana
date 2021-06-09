4b33fceb16c7770de6586e94da3c4b6828e9ac60
Syed Andleeb <andleebsyed75@gmail.com>
2021-06-09 18:25:23 +0530

    refactor : removed unused code

--- src/utils/funcs.js
+++ src/utils/funcs.js
@@ -1,12 +1,6 @@
 import axios from "axios";
 
 export function checkForIdInPlaylist(playlist, videoId) {
-  console.log(
-    "list of videos in our playlist ",
-    playlist,
-    " and video id being ",
-    videoId
-  );
   const ifPresent = playlist.filter((item) => item.id === videoId);
   return ifPresent.length > 0 ? true : false;
 }
