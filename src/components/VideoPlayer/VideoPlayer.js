c30f4f7609c99dd47a4a73e22f0f2449312f22d0
Syed Andleeb <andleebsyed75@gmail.com>
2021-10-14 22:50:40 +0530

    update: auto-play video when opened enabled

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -38,6 +38,7 @@ export function VideoPlayer() {
               controls={true}
               width={"100%"}
               height={"60vh"}
+              playing={true}
             />
           </div>
           <div className="video-label">
